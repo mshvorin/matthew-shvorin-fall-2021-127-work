e7ab201d8b05ffbb5592a1182c487e41e4f6cb96
mshvorin <71457948+mshvorin@users.noreply.github.com>
2021-09-08 20:07:07 -0400

    Add files via upload

---
+++ shapes/repetition.py
@@ -0,0 +1,27 @@
+import turtle
+win=turtle.Screen()
+crush = turtle.Turtle()
+
+for f in range(3):
+  crush.forward(60)
+  crush.left(120)
+
+crush.penup()
+crush.goto(-50,0)
+crush.color("Blue")
+crush.pendown()
+for l in range(4):
+  crush.forward(40)
+  crush.left(90)
+
+crush.penup()
+crush.goto(70,0)
+crush.color("green")
+crush.pendown()
+for w in range(6):
+  crush.forward(50)
+  crush.left(60)
+
+win.exitonclick()
+win.mainloop()
+
